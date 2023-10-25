5e2c8118a2b6f1016b8e573646791f4a7ea3641e
nicolasances <nicolas.matteazzi@gmail.com>
2023-10-25 19:51:03 +0200

    Deployment automation

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM node:12-alpine
+
+RUN mkdir /app
+
+COPY . /app/
+
+WORKDIR /app
+RUN npm install
+
+RUN chmod -R 777 /app
+
+CMD npm run start
--- src/dlg/backup/StartBackup.ts
+++ src/dlg/backup/StartBackup.ts
@@ -19,23 +19,25 @@ export class StartBackup implements TotoDelegate {
         const logger = execContext.logger;
         const cid = execContext.cid ?? correlationId();
         const bucketName = String(process.env.BACKUP_BUCKET);
+
         const today = moment()
+        const twoDaysAgo = moment().add(-2, "days");
 
         let client;
 
         try {
 
             logger.compute(cid, `Starting Database Backup`)
-            
+
             const config = execContext.config as ControllerConfig;
-            
+
             client = await config.getMongoClient();
             const db = client.db(config.getDBName());
 
             // Iterate through the relevant collections
             for (let collection of Object.keys(config.getCollections())) {
 
-                logger.compute(cid, `Starting Backup of collection ${collection}`)
+                logger.compute(cid, `Starting Backup of collection [${collection}]`)
 
                 // Get a cursor to scan that collection
                 const cursor = db.collection(collection).find();
@@ -43,32 +45,36 @@ export class StartBackup implements TotoDelegate {
                 // Define the name of the file, as the current date (YYYYMMDD) followed by the name of the collection
                 const filename = `${today.format("YYYYMMDD")}-${collection}.json`;
 
-                // If a file already exists, delete it
-                fs.rmSync(filename, {force: true})
-
                 // Write line by line
                 while (await cursor.hasNext()) {
-    
+
                     const doc = await cursor.next();
-    
+
                     fs.appendFileSync(filename, JSON.stringify(doc));
 
                 }
 
                 // Upload the file to GCS
                 const bucket = storage.bucket(bucketName)
-                
+
                 await bucket.upload(filename)
 
-                logger.compute(cid, `Backup of collection ${collection} completed and uploaded on Bucket ${bucketName}. Filename [${filename}]`)
-                
+                // Delete the local file
+                fs.rmSync(filename, { force: true })
+
+                // Delete old file on GCS
+                // Name of the file to delete
+                const filenameToDelete = `${twoDaysAgo.format("YYYYMMDD")}-${collection}.json`;
+
+                // Delete old file
+                bucket.file(filenameToDelete).delete({ignoreNotFound: true})
+
+                logger.compute(cid, `Backup of collection [${collection}] completed and uploaded on Bucket [${bucketName}]. Filename [${filename}]`)
+
             }
 
-            // Delete old files
-            // TODO
-
             logger.compute(cid, `Database Backup completed`)
-            
+
             return { backup: "done" }
 
         } catch (error) {
