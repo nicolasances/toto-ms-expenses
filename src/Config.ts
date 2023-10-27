985640a05d03470d437f0732d650aabfc94545ca
nicolasances <nicolas.matteazzi@gmail.com>
2023-10-27 19:58:36 +0200

    Added secrets retrieval for DB

--- src/Config.ts
+++ src/Config.ts
@@ -3,8 +3,10 @@ import { TotoControllerConfig } from "./controller/model/TotoControllerConfig";
 import { ValidatorProps } from "./controller/model/ValidatorProps";
 import { TotoAuthProvider } from "./totoauth/TotoAuthProvider";
 import { MongoClient, ServerApiVersion } from 'mongodb';
+import { SecretManagerServiceClient } from "@google-cloud/secret-manager";
 
-const mongoUrl = "mongodb://" + process.env.MONGO_USER + ":" + process.env.MONGO_PWD + "@" + process.env.MONGO_HOST + ":27017/expenses";
+const secretManagerClient = new SecretManagerServiceClient();
+
 const dbName = 'expenses';
 const collections = {
     expenses: 'expenses',
@@ -15,21 +17,51 @@ const collections = {
 
 export class ControllerConfig implements TotoControllerConfig {
 
+    mongoUser: string | undefined;
+    mongoPwd: string | undefined;
+    mongoHost: string | undefined;
+
+    
     async load(): Promise<any> {
 
+        let promises = [];
+
+        promises.push(secretManagerClient.accessSecretVersion({ name: `projects/${process.env.GCP_PID}/secrets/mongo-host/versions/latest` }).then(([version]) => {
+
+            this.mongoHost = version.payload!.data!.toString();
+
+        }));
+
+        promises.push(secretManagerClient.accessSecretVersion({ name: `projects/${process.env.GCP_PID}/secrets/toto-ms-expenses-mongo-user/versions/latest` }).then(([version]) => {
+
+            this.mongoHost = version.payload!.data!.toString();
+
+        }));
+
+        promises.push(secretManagerClient.accessSecretVersion({ name: `projects/${process.env.GCP_PID}/secrets/toto-ms-expenses-mongo-pswd/versions/latest` }).then(([version]) => {
+
+            this.mongoHost = version.payload!.data!.toString();
+
+        }));
+
+        await Promise.all(promises);
+        
     }
-
+    
     getCustomAuthVerifier(): CustomAuthVerifier {
-
+        
         return new TotoAuthProvider("https://toto-ms-auth-6lv62poq7a-ew.a.run.app")
     }
-
+    
     getProps(): ValidatorProps {
-
+        
         return {}
     }
-
+    
     async getMongoClient() {
+        
+        const mongoUrl = `mongodb://${this.mongoUser}:${this.mongoPwd}@${this.mongoHost}:27017/expenses`
+
         return await new MongoClient(mongoUrl).connect();
     }
 
